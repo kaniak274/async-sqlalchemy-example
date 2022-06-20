4bbce8a0b9f65fb8348d212fa1f25122f50f157c
Kamil Kucharski <kamil.kucharski@makimo.pl>
2022-06-20 18:25:38 +0200

    Add context manager for database integration

--- app/database.py
+++ app/database.py
@@ -1,12 +1,13 @@
 import os
+from contextlib import asynccontextmanager
 
 from sqlalchemy import (
     Column,
     Integer,
     String,
 )
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_async_engine(
     os.environ["DB_URL"],
@@ -22,3 +23,23 @@ class Tutorial(Base):
 
     tutorial_id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
+
+
+def async_session_generator():
+    return sessionmaker(
+        engine, class_=AsyncSession
+    )
+
+
+@asynccontextmanager
+async def get_session():
+    try:
+        async_session = async_session_generator()
+
+        async with async_session() as session:
+            yield session
+    except:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
